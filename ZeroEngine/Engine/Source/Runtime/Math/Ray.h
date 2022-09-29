18f5ce6207190e4c6ae07dc25c750ac4e3d8cb82
zhujieming <zhujieming2003@aliyun.com>
2022-09-29 22:36:49 +0800

    pick actoro by mouse

--- ZeroEngine/Engine/Source/Runtime/Math/Ray.h
+++ ZeroEngine/Engine/Source/Runtime/Math/Ray.h
@@ -23,58 +23,58 @@ namespace Zero
                 return { {0.0f, 0.0f, 0.0f},{0.0f, 0.0f, 0.0f} };
             }
 
-            bool IntersectsAABB(const FAABB& aabb, float& t) const
+            bool IntersectsAABB(const FAABB& AABB, float& T) const
             {
-                ZMath::vec3 dirfrac;
+                ZMath::vec3 DirFrac;
                 // r.dir is unit direction vector of ray
-                dirfrac.x = 1.0f / m_Direction.x;
-                dirfrac.y = 1.0f / m_Direction.y;
-                dirfrac.z = 1.0f / m_Direction.z;
+                DirFrac.x = 1.0f / m_Direction.x;
+                DirFrac.y = 1.0f / m_Direction.y;
+                DirFrac.z = 1.0f / m_Direction.z;
                 // lb is the corner of AABB with minimal coordinates - left bottom, rt is maximal corner
                 // r.org is origin of ray
-                const ZMath::vec3& lb = aabb.Min;
-                const ZMath::vec3& rt = aabb.Max;
-                float t1 = (lb.x - m_Origin.x) * dirfrac.x;
-                float t2 = (rt.x - m_Origin.x) * dirfrac.x;
-                float t3 = (lb.y - m_Origin.y) * dirfrac.y;
-                float t4 = (rt.y - m_Origin.y) * dirfrac.y;
-                float t5 = (lb.z - m_Origin.z) * dirfrac.z;
-                float t6 = (rt.z - m_Origin.z) * dirfrac.z;
+                const ZMath::vec3& lb = AABB.Min;
+                const ZMath::vec3& rt = AABB.Max;
+                float t1 = (lb.x - m_Origin.x) * DirFrac.x;
+                float t2 = (rt.x - m_Origin.x) * DirFrac.x;
+                float t3 = (lb.y - m_Origin.y) * DirFrac.y;
+                float t4 = (rt.y - m_Origin.y) * DirFrac.y;
+                float t5 = (lb.z - m_Origin.z) * DirFrac.z;
+                float t6 = (rt.z - m_Origin.z) * DirFrac.z;
 
-                float tmin = ZMath::max(ZMath::max(ZMath::min(t1, t2), ZMath::min(t3, t4)), ZMath::min(t5, t6));
-                float tmax = ZMath::min(ZMath::min(ZMath::max(t1, t2), ZMath::max(t3, t4)), ZMath::max(t5, t6));
+                float MinT = ZMath::max(ZMath::max(ZMath::min(t1, t2), ZMath::min(t3, t4)), ZMath::min(t5, t6));
+                float MaxT = ZMath::min(ZMath::min(ZMath::max(t1, t2), ZMath::max(t3, t4)), ZMath::max(t5, t6));
 
                 // if tmax < 0, ray (line) is intersecting AABB, but the whole AABB is behind us
-                if (tmax < 0)
+                if (MaxT < 0)
                 {
-                    t = tmax;
+                    T = MaxT;
                     return false;
                 }
 
                 // if tmin > tmax, ray doesn't intersect AABB
-                if (tmin > tmax)
+                if (MinT > MaxT)
                 {
-                    t = tmax;
+                    T = MaxT;
                     return false;
                 }
 
-                t = tmin;
+                T = MinT;
                 return true;
             }
 
-            bool IntersectsTriangle(const ZMath::vec3& a, const ZMath::vec3& b, const ZMath::vec3& c, float& t) const
+            bool IntersectsTriangle(const ZMath::vec3& A, const ZMath::vec3& B, const ZMath::vec3& C, float& T) const
             {
-                const ZMath::vec3 E1 = b - a;
-                const ZMath::vec3 E2 = c - a;
+                const ZMath::vec3 E1 = B - A;
+                const ZMath::vec3 E2 = C - A;
                 const ZMath::vec3 N = cross(E1, E2);
-                const float det = -ZMath::dot(m_Direction, N);
-                const float invdet = 1.f / det;
-                const ZMath::vec3 AO = m_Origin - a;
+                const float Det = -ZMath::dot(m_Direction, N);
+                const float InvDet = 1.f / Det;
+                const ZMath::vec3 AO = m_Origin - A;
                 const ZMath::vec3 DAO = ZMath::cross(AO, m_Direction);
-                const float u = ZMath::dot(E2, DAO) * invdet;
-                const float v = -ZMath::dot(E1, DAO) * invdet;
-                t = ZMath::dot(AO, N) * invdet;
-                return (det >= 1e-6f && t >= 0.0f && u >= 0.0f && v >= 0.0f && (u + v) <= 1.0f);
+                const float U = ZMath::dot(E2, DAO) * InvDet;
+                const float V = -ZMath::dot(E1, DAO) * InvDet;
+                T = ZMath::dot(AO, N) * InvDet;
+                return (Det >= 1e-6f && T >= 0.0f && U >= 0.0f && V >= 0.0f && (U + V) <= 1.0f);
             }
             
             FRay TransformRay (const ZMath::mat4& Transform) const
