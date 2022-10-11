d3c9d91dbe322fffbe3e95f5213eb9d6e7127f9e
zhujieming <zjmn4182@corp.netease.com>
2022-10-11 21:09:27 +0800

    zbt

--- ZeroEngine/Engine/Source/ZeroBuildTool/ZeroBuildTool.cpp
+++ ZeroEngine/Engine/Source/ZeroBuildTool/ZeroBuildTool.cpp
@@ -1,9 +1,31 @@
 #include <iostream> 
 #include "StringUtils.h"
 #include "Config.h"
+#include "Log.h"
 #include "mini/ini.h"
+
+void ParseDir(const std::filesystem::path& Folder)
+{
+	for (auto& Child : std::filesystem::directory_iterator(Folder))
+	{
+		if (std::filesystem::is_directory(Child))
+		{
+			ParseDir(Child.path());
+		}
+		else if (Child.path().extension().string() == ".h")
+		{
+			CLIENT_LOG_INFO(Child.path().string());
+			auto Content = Zero::StringUtils::ReadFile(Child.path().string());
+			std::cout << Content;
+			if ()
+		}
+	}
+}
+
+
 int main()
 {
+	Zero::FLog::Init();
 	std::filesystem::path Path = Zero::Config::ConfigDir / Zero::Config::ZBTFile;
 	std::cout << Path.string() << std::endl;
 	mINI::INIFile File(Path.string());
@@ -14,7 +36,13 @@ int main()
 
 	for (auto const& Iter : Modules)
 	{
-		std::cout << Iter.second << std::endl;
+		//std::cout << Iter.second << std::endl;
+		std::filesystem::path PathModule = Zero::Config::EngineSourceDir / Iter.second;
+		CLIENT_LOG_INFO("Parse Module {0}", PathModule.string());
+		if (std::filesystem::exists(PathModule))
+		{
+			ParseDir(PathModule);
+		}
 	}
 	
 	//file.generate(ini);
