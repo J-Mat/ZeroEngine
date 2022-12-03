f02c5f274c3c6472a7f1c01d7c3e267fd339b35e
zhujieming <zhujieming2003@aliyun.com>
2022-12-03 23:18:45 +0800

    NDF

--- ZeroEngine/Assets/Shader/NDF.hlsl
+++ ZeroEngine/Assets/Shader/NDF.hlsl
@@ -1,12 +1,4 @@
 
-SamplerState gSamPointWrap : register(s0);
-SamplerState gSamPointClamp : register(s1);
-SamplerState gSamLinearWarp : register(s2);
-SamplerState gSamLinearClamp : register(s3);
-SamplerState gSamAnisotropicWarp : register(s4);
-SamplerState gSamAnisotropicClamp : register(s5);
-
-
 struct FDirectLight
 {
     float3 Color;
@@ -31,9 +23,7 @@ cbuffer cbCameraObject : register(b1)
 
 cbuffer cbMaterial : register(b2)
 {
-	float Fresnel_1;
-	float Fresnel_2;
-	float Fresnel_3;
+	float Rougness;
 };
 
 cbuffer cbConstant : register(b3)
@@ -63,7 +53,6 @@ struct PixelOutput
 };
 
 
-
 VertexOut VS(VertexIn vin)
 {
 	VertexOut Vout;
@@ -86,15 +75,34 @@ float3 SchlickFresnel(float3 F0, float3 Normal, float3 LightVec)
     return F0 + (float3(1.0f, 1.0f, 1.0f) - F0) * pow(1.0f - CosTheta, 5.0f);
 }
 
+#define PI 3.1415926
+
+float DistributionGGX(float3 N, float3 H, float a)
+{
+	float NdotH = max(dot(N, H), 0.0f);
+	float NdotH2 = NdotH * NdotH;
+	
+	float a2 = a * a;
+	float denom = (NdotH2 * (a2 - 1.0f) + 1.0f);
+	denom = PI * denom * denom;
+	
+	return a2 / denom;
+}
+
+
+
 PixelOutput PS(VertexOut Pin)
 {
 	PixelOutput Out;
 	float3 LightColor = DirectLights[0].Color * DirectLights[0].Intensity;
-	float3 LightVec = -DirectLights[0].Direction;
-	float NdotL = saturate(dot(Pin.Normal, LightVec));
-
-	//float3 Fresnel = float3(Fresnel_1, Fresnel_2, Fresnel_3);
-	//float3 Color = 	SchlickFresnel(Fresnel, Pin.Normal, LightVec);
-	Out.BaseColor = float4(float3(NdotL, NdotL, NdotL), 1.0f);
+	float3 L = normalize(-DirectLights[0].Direction);
+	float3 V = normalize(ViewPos - Pin.WorldPos); 
+	float3 H = normalize(V + L);
+	float NdotL = saturate(dot(L, Pin.Normal));
+	float NDF = DistributionGGX(Pin.Normal, H, Rougness);
+	float NdotH = max(dot(Pin.Normal, H), 0.0f);
+	Out.BaseColor = float4(float3(NDF, NDF, NDF), 1.0f);
+	//Out.BaseColor = float4(float3(NDF, NDF, NDF), 1.0f);
+	//Out.BaseColor = float4(float3(Rougness, Rougness, Rougness), 1.0f);
 	return Out;
 }
